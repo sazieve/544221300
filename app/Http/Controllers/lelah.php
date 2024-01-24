a2d95ded08fbd81b7cdff3982d14b335eee02a83
sazieve <raisazkabenazir@gmail.com>
2024-01-25 06:33:33 +0700

    first commit

--- app/Http/Controllers/lelah.php
+++ app/Http/Controllers/lelah.php
@@ -1,49 +1,35 @@
 <?php
- namespace App\Http\Controllers;
 
- use Illuminate\Http\Request;
- use Illuminate\App\Models\Admin;
+namespace App\Http\Controllers;
 
- class adminController extends Controller
- {
+use Illuminate\Http\Request;
+use App\Http\Models\Buku;
+
+class adminController extends Controller
+{
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-   public function index()
-   {
-       $admin = Admin::all();
-       return view ('index')->with('admin', $admin);
-   }
+    public function index()
+    {
+        $admin = Admin::all();
+        return view ('index')-> with('admin', $buku);
+    }
 
-   /**
-    * Show the form for creating a new resource.
-    *
-    *@return \Illuminate\Http\Response
-    */
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('create');
     }
-
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request
      */
-    public function store(Request $request)
-    {
-        $input = $request->all();
-        Admin::create($input);
-        return redirect('admin')->with('flash_message', 'Makanan telah ditambahkan! ');
-        /**
-         * Update the specified resource in storage.
-         *
-         * @param \Illuminate\Http\Request
-         * @param int $id
-         * @return mixed
-         */
-    }
- }
+}
